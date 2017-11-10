1b7a15260cd7f1bfd119880354a61893f8cb0ca5
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2017-11-10 11:42:17 +0100

    Draw a rectangle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -73,6 +73,7 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
 	return id;
 }
 
+// TODO rename to create program
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
 	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
@@ -118,24 +119,28 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	// buffer data
-	float positions[6] = {
+	float positions[12] = {
 		-0.5f, -0.5f, // single vertex
-		0.0f, 0.5f,
-		0.5f, -0.5f
+		0.5f, -0.5f,
+		0.5f, 0.5f,
+
+		0.5f, 0.5f,
+		-0.5f, 0.5f,
+		-0.5f, -0.5f
 	};
 
 	/* Generate buffer */
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	/* Buffer layout specification */
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
+	/* Create program from shader file */
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-
  	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
  	glUseProgram(shader);
 
@@ -145,7 +150,7 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
